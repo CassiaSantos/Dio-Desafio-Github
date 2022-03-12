9b4c7c5a5f73c0f6b836b1bdb0dffe7539c44c8a
CassiaSantos <cassia.oliveira.profissional@gmail.com>
2022-03-11 23:01:37 -0300

    JS avançado - Arrow Functions

---
+++ JavaScript ES6 avancado/JS-Avancado.js
@@ -0,0 +1,18 @@
+Em Js, as funções são de 1ª classe, ou seja, podem ser passadas como parâmetros par outras funções.
+
+ARROW FUNCTIONS: =>
+- são funções anonimas, em que o nome é omitido, porém é necessário atribuílas a uma variável ou passando como parâmetro para outra função. 
+- o return pode ser omitido se houver apenas 1 linha de comando, ou seja, retornar o resultado de uma expressão.
+- se houver mais de 1 statementes (linhas de código), é necessário escrever o código entre chaves.
+- se houver mais de um argumento, deverão estar entre parenteses. 
+
+var sum = (a, b) => a + b;
+
+var sum = (a, b) => {
+	if () {
+		// code...
+	}
+	
+	return ;
+}
+
